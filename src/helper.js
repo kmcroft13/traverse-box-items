fbedcb56033a634a6a6f6e1840218a1cbada30f9
Kyle Croft <k.croft@me.com>
2019-11-08 22:51:48 -0600

    Created "helper" module with new execution ID func

---
+++ src/helper.js
@@ -0,0 +1,7 @@
+const generateExecutionId = function() {
+    const execIdLength = 10;
+    const execId = [...Array(execIdLength)].map(_=>(Math.random()*36|0).toString(36)).join``;
+    return execId;
+}
+
+module.exports = { generateExecutionId };
